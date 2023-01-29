489a005a138c5d12750f6afbe7805de0f8c44b7a
Lipari <lipari.giuseppe@gmail.com>
2023-01-29 10:58:18 +0100

    class refactorization

---
+++ configs.py
@@ -0,0 +1,6 @@
+PATH_DIRECTORY_TEST = "./testAnalyze"
+PATH_DIRECTORY_OUTPUT = "./outputMatrix"
+NAME_FILE_MATRIX_INCIDENCE = "incidenceMatrix"
+NAME_FILE_SOLUTION = "xSolutionVector"
+NAME_FILE_TIME = "times"
+ACTIVE_DRAW_GRAPH = 0

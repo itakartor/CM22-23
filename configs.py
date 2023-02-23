d02a3df81c71852e0396a7e113021c499e1612a3
liparig <lipari.giuseppe@gmail.com>
2023-02-23 22:06:06 +0100

    dmx file and add path in configs

--- configs.py
+++ configs.py
@@ -1,4 +1,6 @@
 PATH_DIRECTORY_TEST = "./testAnalyze"
+PATH_DMX = "./generators/dmx"
+
 PATH_DIRECTORY_OUTPUT = "./outputMatrix"
 NAME_FILE_MATRIX_INCIDENCE = "incidenceMatrix"
 NAME_FILE_SOLUTION = "xSolutionVector"
